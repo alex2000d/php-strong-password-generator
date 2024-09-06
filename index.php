0db374f60ad3b36fadd6f725993bd3ad00fb459e
alex2000d <demarcoalessandro777@gmail.com>
2024-09-06 17:01:46 +0200

    [UPD] created the function

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+     if (isset($_GET["lunghezza_password"])){
+          // creo una variabile che chiamo lunghezza
+        $lunghezza = $_GET["lunghezza_password"];
 
+          // creo la mia funzione per generare la password randomicamente
+           function createdPassword($lunghezza){
+            // creo la variabile che contiene la stringa con i caratteri
+             $caratteri ="abcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZ123456789?!&/%";
+              $password = "";
+            //   creo un for per ciclare i miei caratteri randomicamente
+               for ($i = 0; $i < $lunghezza; $i++){
+                // creo una variabile per la selezione random della lunghezza della mia stringa meno 1
+                 $selection = rand(0,strlen($caratteri) -1);
+                //  concateno tutto qui
+                 $password .= $caratteri[$selection];
+
+               }
+               // restituisco il risultato
+                 return $password;
+                 echo $password;
+           }
+     }
 ?>
 
 <!DOCTYPE html>
@@ -21,15 +42,16 @@
                   <form action="./index.php" method="GET">
                       <div class="form-group mt-5">
                                 <div class="control-label">lunghezza password</div>
-                                <input type="number" class="form-control" name="lunghezza" placeholder="inserisci la lunghezza">
+                                <input type="number" class="form-control" name="lunghezza_password" placeholder="inserisci la lunghezza">
                             </div>
                       </div>
                       <div class="col-12">
                             <button type="submit" class="btn btn-primary mt-5">invia</button>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </main>
-</body>
+                      </div>
+                      <div><?php echo $password?></div>
+                 </div>
+             </div>
+          </div>
+      </main>
+  </body>
 </html>
